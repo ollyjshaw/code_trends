46c730143ac5127bbddc96e947d6f357d481d75b
Olly Shaw <ollyjshaw@gmail.com>
2014-04-17 14:43:49 +0100

    Comments formatting etc

--- test/routes/RoutesTest.java
+++ test/routes/RoutesTest.java
@@ -1,18 +1,5 @@
 package routes;
 
-import org.junit.Test;
-import play.libs.F.Callback;
-import play.mvc.Result;
-import play.test.TestBrowser;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.*;
-
 public class RoutesTest {
-
-//    @Test
-//    public void submitRoute() {
-//        Result route = routeAn
-//    }
 
 }
