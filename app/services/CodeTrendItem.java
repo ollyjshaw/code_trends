e5230f96724573883e7433e297f807fa5add0b6f
AurimasM <aurimas.mileikis@accenture.com>
2014-04-17 13:27:12 +0100

    Random Analytics

--- app/services/CodeTrendItem.java
+++ app/services/CodeTrendItem.java
@@ -2,12 +2,18 @@ package services;
 
 public class CodeTrendItem {
     private final String language;
-    public CodeTrendItem(String language) {
+    private final Double popularity;
+    
+    public CodeTrendItem(String language, Double popularity) {
         this.language = language;
-        //To change body of created methods use File | Settings | File Templates.
+        this.popularity = popularity; 
     }
 
     public String getLanguage() {
         return language;
+    }
+    
+    public Double getPopularity(){
+    	return popularity;
     }
 }
---
+++ app/services/RandomCodeTrendAnalytics.java
@@ -0,0 +1,32 @@
+package services;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomCodeTrendAnalytics implements CodeTrendAnalytics{
+
+	@Override
+	public List<CodeTrendItem> getCodeTrends(String[] input) {
+
+		List<CodeTrendItem> result = new LinkedList<CodeTrendItem>();
+		
+		double[] randArray= new double[input.length];
+		double total = 0D;
+		
+		for(int i=0; i<input.length; ++i){
+			randArray[i]= Math.random();
+			total+=randArray[i];
+		}
+		
+		double indValue = 100.00/total;
+		
+		for(int j=0; j<input.length; ++j){
+			double trendValue = randArray[j]*indValue;
+			CodeTrendItem item = new CodeTrendItem(input[j], trendValue);
+            result.add(item);
+		}
+		
+		return result;
+	}
+}
