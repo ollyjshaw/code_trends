15ac038a44aa504572ab796a258b0807169fc560
unknown <christopher.reynolds@MW7V05AEEJ2EFE.dir.svc.accenture.com>
2014-04-17 11:53:36 +0100

    input view refactoring with scala

---
+++ app/models/InputForm.java
@@ -0,0 +1,28 @@
+package models;
+
+public class InputForm {
+
+    public String language1;
+    public String language2;
+    public String language3;
+
+
+
+//    public InputForm(String language1, String language2, String language3) {
+//        this.language1 = language1;
+//        this.language2 = language2;
+//        this.language3 = language3;
+//    }
+//
+//    public String getLanguage1() {
+//        return language1;
+//    }
+//
+//    public String getLanguage2() {
+//        return language2;
+//    }
+//
+//    public String getLanguage3() {
+//        return language3;
+//    }
+}
--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import models.InputForm;
 import org.junit.*;
 
+import play.data.Form;
 import play.mvc.*;
 import play.test.*;
 import play.data.DynamicForm;
@@ -27,6 +29,8 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationTest {
 
+    private static final Form<InputForm> inputForm = Form.form(InputForm.class);
+
     @Test
     public void simpleCheck() {
         int a = 1 + 1;
@@ -42,24 +46,24 @@ public class ApplicationTest {
 	
 	@Test
     public void renderInputTemplate() {
-        Content html = views.html.input.render();
+        Content html = views.html.input.render(inputForm);
         assertThat(contentType(html)).isEqualTo("text/html");
 		assertThat(contentAsString(html)).contains("Search Twitter popularity");
     }
 
     @Test
     public void checkInputViewHasTitle() {
-        Content html = views.html.input.render();
+        Content html = views.html.input.render(inputForm);
         assertThat(contentAsString(html)).contains("title for input view");
     }
 
-	// Check that the input page contains 3 text fields and a button
 	@Test
 	public void checkHTMLInputFieldsExist() {
-		Content html = views.html.input.render();
-		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"trend1\" name=\"trend1\" value=\"\">");
-		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"trend2\" name=\"trend2\" value=\"\">");
-		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"trend3\" name=\"trend3\" value=\"\">");
+		Content html = views.html.input.render(inputForm);
+		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"language1\" name=\"language1\" value=\"\" >");
+		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"language2\" name=\"language2\" value=\"\" >");
+		assertThat(contentAsString(html)).contains("<input type=\"text\" id=\"language3\" name=\"language3\" value=\"\" >");
+
 		assertThat(contentAsString(html)).contains("<input class=\"button\" type=\"submit\" value=\"View trends\">");
 	}
 
