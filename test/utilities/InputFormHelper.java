2ae024a0f1557ad18f2cb0153dca586ad83f0d8e
AurimasM <aurimas.mileikis@accenture.com>
2014-04-30 14:58:24 +0100

    Add Selenium tests and input form helpers

---
+++ test/utilities/InputFormHelper.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class InputFormHelper {
+	
+	//Default form values
+	public final String LANGUAGE1_DEFAULT = "Java";
+	public final String LANGUAGE2_DEFAULT = "C#";
+	public final String LANGUAGE3_DEFAULT = "Scala";
+	
+	//Default form ids
+	public static final String LANGUAGE1_ID = "language1"; 
+	public static final String LANGUAGE2_ID = "language2";
+	public static final String LANGUAGE3_ID = "language3";
+	public static final String BUTTON_ID = "view_trends";
+	
+	public static WebDriverHelper webDriverHelper = new WebDriverHelper();
+	
+	public void populateInputFormTextFields(WebDriver driver, String language1, String language2, String language3){
+		
+		webDriverHelper.clearAndPopulateTextFieldByName(driver, LANGUAGE1_ID, language1);
+		webDriverHelper.clearAndPopulateTextFieldByName(driver, LANGUAGE2_ID, language2);
+		webDriverHelper.clearAndPopulateTextFieldByName(driver, LANGUAGE3_ID, language3);	
+	}
+	
+	public void populateDefaultLaguageDetails(WebDriver driver){
+		
+		populateInputFormTextFields(driver, LANGUAGE1_DEFAULT, LANGUAGE2_DEFAULT, LANGUAGE3_DEFAULT);
+	}
+	
+	public void clickSubmitButton(WebDriver driver){
+		
+		driver.findElement(By.id(BUTTON_ID)).click();
+	}
+	
+}
