4cbf903a843c3b829edd55a7ed5a7edabaac4947
unknown <aivars.smaukstelis@LVALTP0984.dir.svc.accenture.com>
2014-04-30 17:25:36 +0100

    formatted JSON response

--- test/views/TrendDataTest.java
+++ test/views/TrendDataTest.java
@@ -1,19 +1,17 @@
 package views;
 
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 import play.mvc.Content;
 import services.CodeTrendItem;
 import view_models.CodeTrendViewModel;
-import views.html.trend_result;
 import views.txt.trend_data;
-
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.contentType;
 
 public class TrendDataTest {
 
@@ -25,8 +23,8 @@ public class TrendDataTest {
         items.add(new CodeTrendItem("Scala", 20.66666d));
         CodeTrendViewModel model = new CodeTrendViewModel(items);
         Content txt = trend_data.render(model);
-        assertThat(contentAsString(txt)).contains("{language:\"Ruby\", score:20.00}");   	
-        assertThat(contentAsString(txt)).contains("{language:\"Java\", score:60.33}");  
-        assertThat(contentAsString(txt)).contains("{language:\"Scala\", score:20.67}");  
+        assertThat(contentAsString(txt)).contains("{language:\"Ruby\",score:20.00}");   	
+        assertThat(contentAsString(txt)).contains("{language:\"Java\",score:60.33}");  
+        assertThat(contentAsString(txt)).contains("{language:\"Scala\",score:20.67}");  
     }
 }
